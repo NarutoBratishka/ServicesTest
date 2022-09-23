4e9a6f0d2868cefdbeb47020d0b8ebd6a3b52f92
NarutoBratishka <katorabian@gmail.com>
2022-09-23 16:55:37 +0700

    add: notification for sdk >= 26

--- app/src/main/java/ru/sumin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -1,7 +1,12 @@
 package ru.sumin.servicestest
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import ru.sumin.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +22,34 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService( MyService.newIntent(this, 25) )
         }
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+//            startService( MyService.newIntent(this, 25) )
+        }
+    }
+
+    private fun showNotification() {
+        val notMan = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notChan = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notMan.createNotificationChannel(notChan)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+
+        notMan.notify(1, notification)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "Foreground"
     }
 }
